74867ae20173a2d4a86ada51800c9c38c4466d8f
AndjelaDamnjanovic <mi19059@alas.matf.bg.ac.rs>
2024-12-06 16:27:14 +0100

    Tried predicting disorder based on repeats

--- code/repeatsToDatabase.py
+++ code/repeatsToDatabase.py
@@ -1,6 +1,10 @@
 import sqlite3
 try:
-    conn = sqlite3.connect('../sequences/sequences2.db')
+    #conn = sqlite3.connect('../repeats/repeats5.db')
+    #conn = sqlite3.connect('../repeats/repeats4.db')
+    #conn = sqlite3.connect('../repeats/repeats3.db')
+    #conn = sqlite3.connect('../repeats/repeats2.db')
+    conn = sqlite3.connect('../repeats/repeats1.db')
     cursor = conn.cursor()
     print("Database created and Successfully Connected to SQLite")
 
@@ -8,14 +12,18 @@ try:
     cursor.execute(sqlite_select_Query)
     record = cursor.fetchall()
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS sequenceMap (
+        CREATE TABLE IF NOT EXISTS repeatMap1 (
         SEQUENCE TEXT PRIMARY KEY,
         d INTEGER DEFAULT  0,
         i INTEGER DEFAULT  0,
         o INTEGER DEFAULT  0
         )
         ''')
-    with open('../sequences/seq2.txt', 'r') as file:
+    with open('../repeats/directRepeats1.txt', 'r') as file:
+    #with open('../repeats/directRepeats2.txt', 'r') as file:
+    #with open('../repeats/directRepeats3.txt', 'r') as file:
+    #with open('../repeats/directRepeats4.txt', 'r') as file:
+    #with open('../repeats/directRepeats5.txt', 'r') as file:
         i = 0
         for line in file:
             # Parse the line to extract the necessary fields
@@ -23,14 +31,13 @@ try:
             #field1, field2, field3 = line.strip().split(',')
             parts = line.strip().split(':')
             key = parts[0].strip()
-            values = parts[1].replace('[', '').replace(']', '').replace('  ', ' ').strip().split(' ')
-            #print(values)
+            values = parts[1].replace('[', '').replace(']', '').split() #.replace('  ', ' ').strip().split(' ')
 
             i+= 1
             if (i%1000000 == 0):
                 print(i/1000000)
             
-            cursor.execute('SELECT d, i, o FROM sequenceMap WHERE SEQUENCE = ?', (key,))
+            cursor.execute('SELECT d, i, o FROM repeatMap1 WHERE SEQUENCE = ?', (key,))
             result = cursor.fetchone()
         
             if result:
@@ -42,22 +49,33 @@ try:
             
                 # Update the record
                 cursor.execute('''
-                UPDATE sequenceMap
+                UPDATE repeatMap1
                 SET d = ?, i = ?, o = ? 
                 WHERE SEQUENCE = ?
                 ''', (d_new, i_new, o_new, key))
             else:
                 # Record does not exist, insert new record
                 count = cursor.execute('''
-                INSERT INTO sequenceMap (SEQUENCE, d, i, o) 
+                INSERT INTO repeatMap1 (SEQUENCE, d, i, o) 
                 VALUES (?, ?, ?, ?)
                 ''', (key, int(values[0]), int(values[1]), int(values[2])))
              #   conn.commit()
-            #    print("Record inserted successfully into sequenceMap table ", cursor.rowcount)
+            #    print("Record inserted successfully into repeatMap1 table ", cursor.rowcount)
 
                 
     # Commit the changes and close the connection
     conn.commit()
+    #sqlite_select_query = """SELECT * from repeatMap1"""
+    #cursor.execute(sqlite_select_query)
+    #records = cursor.fetchall()
+    #print("Total rows are:  ", len(records))
+    #print("Printing each row")
+    #for row in records:
+    #    print("sequence: ", row[0])
+    #    print("d: ", row[1])
+    #    print("i: ", row[2])
+    #    print("o: ", row[3])
+    #    print("\n")
     cursor.close()  # Close the cursor explicitly
     #conn.close()
     
---
+++ code/testRepeats.py
@@ -0,0 +1,82 @@
+from typing import List
+from Bio import SeqIO
+import numpy as np
+import json
+import sqlite3
+
+
+def processResults(records, threshold, i, n, disorderPredictor, f, id):
+    if not records:
+        return
+    numInstances = records[0] + records[1] + records[2]
+    disorderPercentage = float((records[0] + records[1])/numInstances)
+    #intersectPercentage = float(records[1]/numInstances)
+    orderPercentage = float(records[2]/numInstances)
+
+    # IMA DOSTA NISKI SA DISORDER % PREKO 80% 
+    mostFrequent = max(disorderPercentage, orderPercentage)
+    if disorderPercentage == mostFrequent and disorderPercentage >= threshold:
+        #f.write(id + ": " + str(i) + " - " + str(n) + "\n")
+        for j in range(i, n):
+            disorderPredictor[j][0] += 1
+    elif orderPercentage == mostFrequent and orderPercentage >= threshold:
+        for j in range(i, n):
+            disorderPredictor[j][2] += 1
+    #elif intersectPercentage == mostFrequent and intersectPercentage >= threshold:
+    #    for j in range(i, n):
+    #        disorderPredictor[j][1] += 1 
+
+def disorderToString(disorderPredictor):
+    res = ""
+    # BICE PROBLEMA AKO IMA VISE MAKSIMUMA, NISKA CE BITI DUZA NEGO STO TREBA
+    # NADJI NEKO RESENJE DA SE ODLUCUJE, NPR AKO JE BILO DISORDERA DO NJE, VRV
+    # JE I TU DISORDER...
+    for l in disorderPredictor:
+        maxElem = max(l[0], l[1], l[2])
+        if l[0] == maxElem:
+            res = res + "d"
+        elif l[1] == maxElem:
+            res = res + "i"
+        else:
+            res = res + "o"
+    return res
+
+def extractSequences(testSet, threshold, outfile):
+    try:
+        fasta_sequences = SeqIO.parse(open(testSet), 'fasta')
+        conn = sqlite3.connect('../repeats/repeats1.db')
+        cursor = conn.cursor()
+        print("Database created and Successfully Connected to SQLite")
+
+        sqlite_select_Query = "select sqlite_version();"
+        cursor.execute(sqlite_select_Query)
+        record = cursor.fetchall()
+        f = open(outfile, "w")
+        for fasta in fasta_sequences:
+            name, sequence = fasta.id, str(fasta.seq)
+            disorderPredictor = [[0, 0, 0] for i in range(len(sequence))]
+            seq_len = len(sequence)
+            print(name)
+            for n in range(3, seq_len + 1):
+                for i in range(seq_len - n + 1):     
+                    cursor.execute('SELECT d, i, o FROM repeatMap1 WHERE SEQUENCE = ?', (sequence[i:i + n],))
+                    records = cursor.fetchone()    
+                    processResults(records, threshold, i, n + i, disorderPredictor, f, name)
+            
+            #print(disorderPredictor)
+            res = disorderToString(disorderPredictor) 
+            disorderPredictor.clear() 
+            f.write(name + " " + res + "\n")
+        f.close()          
+            
+        cursor.close()  # Close the cursor explicitly
+        print("Data has been successfully stored in the SQLite database.")
+    except sqlite3.Error as error:
+        print("Error while connecting to sqlite", error)
+    finally:
+        if conn:
+            conn.close()
+        print("The SQLite connection is closed") 
+
+#extractSequences("../testSets/testSet1.fasta")
+extractSequences("../testSets/testSet1.fasta", 0.80, "../results/probaRepeats1.txt")
